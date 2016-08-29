4c275e9955ab6bf8f7d4be957970e4c4dd021d35
xinzhi <1123858432@qq.com>
2016-08-29 17:31:26 +0800

    架子

---
+++ zxz.js
@@ -0,0 +1,18 @@
+(function(w){
+    //
+    var Z = function(){
+
+    };
+
+    //添加方法
+    Z.prototype = {
+    //正则模板
+    template: function(id){
+    
+    }
+        
+    }
+
+    //抛出库
+    window.Z = new Z();
+})(window)
