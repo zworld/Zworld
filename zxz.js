ece3540bdc0ba7d99ae2c0dfa4490b2762bbf7af
zhongxinzhi <1123858432@qq.com>
2016-09-28 18:10:41 +0800

    格式化日期

--- zxz.js
+++ zxz.js
@@ -59,12 +59,8 @@
     Z.isPhone = function(param){
         var phone = param.toString();
         console.log(typeof(param))
-        var reg = /^[(13)(15)(18)]{1}[0-9]{10}$/;
-        if(!reg.test(phone))
-            return false
-        else
-            return true
-        debugger
+        var reg = /^1[34578][0-9]{10}$/;
+        return reg.test(phone)
     }
 
     //身份证
@@ -87,6 +83,39 @@
         else
             return false
     } 
+    //验证URL
+    Z.isUrl = function(param){
+        var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" // ftp的user@
+        + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+        + "|" // 允许IP和DOMAIN（域名）
+        + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+        + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+        + "[a-z]{2,6})" // first level domain- .com or .museum
+        + "(:[0-9]{1,4})?" // 端口- :80
+        + "((/?)|" // a slash isn't required if there is no file name
+        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+        var re = new RegExp(strRegex);
+        return re.test(param);
+    }
+    /*日期format*/
+    Date.prototype.format = function(fmt){
+        this.fmt = fmt;
+        var reg = {
+            "y+": this.getFullYear(),
+            "m+": this.getMonth() + 1,
+            "d+": this.getDate()
+        }
+        for(var r in reg){
+            if(new RegExp( "(" + r + ")","gi").test(fmt)){
+                fmt = fmt.replace(RegExp.$1,reg[ r ] )
+            }
+        }
+        
+        return fmt
+
+    }
+
 
     //抛出库
     window.Z = Z;
